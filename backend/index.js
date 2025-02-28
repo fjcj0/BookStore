d2df57be2a3fe12bcab0db6e840524c886103581
omar <omarqais225@gmail.com>
2025-03-01 01:12:47 +0200

    store and get books

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,51 @@
 import express from "express";
-
+import { Book } from './models/bookModel.js';
 import { PORT, mongoDBURL } from "./config.js";
-
 import mongoose from "mongoose";
 
-
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send("Welcome to this page");
 });
 
+app.post('/books', async (request, response) => {
+    try {
+        if (!request.body.title || !request.body.author || !request.body.publishYear) {
+            return response.status(400).send({ message: "All fields are required" });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+        const book = await Book.create(newBook);
+        return response.status(201).send({ message: "The book is created successfully. Congratulations!", data: book });
+    } catch (error) {
+        console.log(error.message);
+        return response.status(500).send({ message: error.message });
+    }
+});
+
+app.get('/books', async (request, response) => {
+    try {
+        const books = await Book.find({});
+        return response.status(200).json({ count: books.length, data: books });
+    } catch (error) {
+        console.log(error.message);
+        return response.status(500).send({ message: error.message });
+    }
+});
+
 app.listen(PORT, () => {
-    console.log(`the server is running on port ${PORT}`);
+    console.log(`The server is running on port ${PORT}`);
 });
 
 mongoose.connect(mongoDBURL).then(() => {
-    console.log('database is connected');
-
+    console.log('Database is connected');
 }).catch((error) => {
     console.log(error.message);
 });
